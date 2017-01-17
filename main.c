30a1a4c26942523f2511fa9371c840006133cdbd
Benjamin Zaslavsky <bzaslavsky@omnilog.fr>
2017-01-17 10:02:00 +0100

    First test with scanf and conditions

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int age = 0;
+    printf("Entrez votre age:\n");
+    scanf("%d", &age);
+    if (age >= 18) {
+        printf("Ah, vous avez %d ans, vous etes majeur!\n\n", age);
+    } else {
+        printf("Oh, vous avez seulement %d ans? Pas le droit de voter!\n\n", age);
+    }
     return 0;
 }
