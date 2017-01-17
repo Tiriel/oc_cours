b400da80713a4f2adf50b28404284812f59b6307
Benjamin Zaslavsky <bzaslavsky@omnilog.fr>
2017-01-17 14:46:26 +0100

    First activity

--- main.c
+++ main.c
@@ -1,14 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-    int age = 0;
-    printf("Entrez votre age:\n");
-    scanf("%d", &age);
-    if (age >= 18) {
-        printf("Ah, vous avez %d ans, vous etes majeur!\n\n", age);
-    } else {
-        printf("Oh, vous avez seulement %d ans? Pas le droit de voter!\n\n", age);
-    }
+    const int MIN  = 1;
+    int continuer = 0, max = 100;
+
+    srand(time(NULL));
+
+    do {
+        int devine = 0, diff = 1;
+
+        do {
+            printf("Choisissez une difficulté :\n");
+            printf("1 - facile (max 100)\n");
+            printf("2 - moyen (max 1000)\n");
+            printf("3 - difficile (max 10000)\n");
+            scanf("%d", &diff);
+            switch (diff) {
+                case 1:
+                    max = 100;
+                    break;
+                case 2:
+                    max = 1000;
+                    break;
+                case 3:
+                    max = 10000;
+                    break;
+                default:
+                    printf("Je n'ai pas compris, recommencez.");
+            }
+        } while (diff > 3);
+        int nombreMystere = (rand() % (max - MIN + 1)) + MIN, coups = 0;
+
+
+        do {
+            printf("Devinez le nombre: ");
+            scanf("%d", &devine);
+            if (devine < nombreMystere) {
+                printf("C'est plus!\n");
+            } else if (devine > nombreMystere) {
+                printf("C'est moins!\n");
+            } else {
+                printf("Bravo! Vous avez trouvé le nombre mystère en %d coups!\n", coups);
+            }
+            coups++;
+        } while (devine != nombreMystere);
+        printf("Encore une partie? 1 = Oui, 0 = Non");
+        scanf("%d", &continuer);
+    } while (continuer);
+
     return 0;
 }
